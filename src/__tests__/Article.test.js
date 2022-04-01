f580624388e6727755d9fd2d0b790557ca598918
WilliamDenega <williamdenega@gmail.com>
2022-04-01 13:11:19 -0400

    commit tree 0f4a0a6fefd0964b4f7553ef9c2b81f0982a5b58

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,12 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import { elementRoles } from "aria-query";
 import React from "react";
 
 import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
-
+  screen.debug()
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
